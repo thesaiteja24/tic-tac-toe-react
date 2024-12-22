18c4383d8f6efa443da31a1955b7eb480d815efe
TheSaiTeja <iamsaiteja2411@gmail.com>
2024-12-22 16:09:04 +0530

    update meta data

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,27 @@ function App() {
   return (
     <>
       <Helmet>
+        {/* Page title */}
         <title>TIC-TAC-TOE</title>
-        <link
-          rel="icon"
-          type="image/png"
-          href="/logo.png"
-        />
+
+        {/* Favicon */}
+        <link rel="icon" type="image/png" href="/logo.png" />
+
+        {/* Meta description */}
+        <meta name="description" content="Play the classic Tic-Tac-Toe game online!" />
+
+        {/* Open Graph meta tags for social media previews */}
+        <meta property="og:title" content="TIC-TAC-TOE" />
+        <meta property="og:description" content="Challenge your friends in this classic Tic-Tac-Toe game!" />
+        <meta property="og:image" content="/logo.png" />
+        <meta property="og:url" content="https://your-site-link.com" />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter meta tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="TIC-TAC-TOE" />
+        <meta name="twitter:description" content="Play the classic Tic-Tac-Toe game with a modern twist!" />
+        <meta name="twitter:image" content="/logo.png" />
       </Helmet>
       <Board />
     </>
