7ab16da1ff205cc9802766a9a3048201c149f8a9
TheSaiTeja <iamsaiteja2411@gmail.com>
2024-12-22 15:52:40 +0530

    update header title and favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import React from "react";
+import { Helmet } from "react-helmet";
 import "./App.css";
 import Board from "./components/Board";
 
 function App() {
   return (
     <>
+      <Helmet>
+        <title>TIC-TAC-TOE</title>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/logo.png"
+        />
+      </Helmet>
       <Board />
     </>
   );
